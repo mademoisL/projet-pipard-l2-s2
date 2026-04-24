e47d8e09b4957c250e6b07295696684645f29826
mademoisL <l.amazone17.03@gmail.com>
2026-04-24 13:03:28 +0200

    test pour corriger l'affichage du profil

--- HTML/Profil.php
+++ HTML/Profil.php
@@ -146,7 +146,7 @@ $hq = $pdo->prepare("SELECT action,date_action FROM historique WHERE utilisateur
                     <li>
                         <span style="color:<?= $couleur ?>; margin-right:10px;">●</span>
                         <?= htmlspecialchars($c['type_appareil']) ?>
-                        <span style="float:right; color:#888; font-size:0.8rem;"><?= $c['etat'] ?></span>
+                        <span style="color:#888; font-size:0.8rem;"><?= $c['etat'] ?></span>
                     </li>
                 <?php endforeach; ?>
             </ul>
